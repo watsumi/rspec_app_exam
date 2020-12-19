7885983b6d62acd755044f7934a58e054feb72b3
watsumi <w.utsumi2@gmail.com>
2020-12-19 13:24:38 +0900

    Fix:テストコードの修正

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'Task', type: :system do
 let(:project) { create(:project) }
-let(:task) { create(:task, project_id: project.id) }
+let(:task) { create(:task) }
   describe 'Task一覧' do
     context '正常系' do
-      let!(:task) { create(:task, project_id: project.id) }
+      let!(:task) { create(:task) }
       it '一覧ページにアクセスした場合、Taskが表示されること' do
         # TODO: ローカル変数ではなく let を使用してください
         visit project_tasks_path(project)
@@ -64,7 +64,7 @@ let(:task) { create(:task, project_id: project.id) }
         fill_in 'Deadline', with: Time.current
         click_button 'Update Task'
         click_link 'Back'
-        expect(find('.task_list')).to have_content(Time.current.strftime('%m/%d %H:%M'))
+        expect(find('.task_list')).to have_content(short_time(Time.current))
         expect(current_path).to eq project_tasks_path(project)
       end
 
@@ -80,13 +80,13 @@ let(:task) { create(:task, project_id: project.id) }
 
       it '既にステータスが完了のタスクのステータスを変更した場合、Taskの完了日が更新されないこと' do
         # TODO: FactoryBotのtraitを利用してください
-        task = create(:task, project_id: project.id, status: :done, completion_date: Time.current.yesterday)
-        visit edit_project_task_path(project, task)
+        completion_task = create(:task, :completion_task)
+        visit edit_project_task_path(project, completion_task)
         select 'todo', from: 'Status'
         click_button 'Update Task'
         expect(page).to have_content('todo')
         expect(page).not_to have_content(Time.current.strftime('%Y-%m-%d'))
-        expect(current_path).to eq project_task_path(project, task)
+        expect(current_path).to eq project_task_path(project, completion_task)
       end
     end
   end
@@ -94,12 +94,12 @@ let(:task) { create(:task, project_id: project.id) }
   describe 'Task削除' do
     context '正常系' do
       # FIXME: テストが失敗するので修正してください
-      let!(:task) { create(:task, project_id: project.id) }
+      let!(:task) { create(:task) }
       it 'Taskが削除されること' do
         visit project_tasks_path(project)
         click_link 'Destroy'
         page.driver.browser.switch_to.alert.accept
-        expect(page).not_to have_content task.title
+        expect(find('.task_list')).not_to have_content task.title
         expect(Task.count).to eq 0
         expect(current_path).to eq project_tasks_path(project)
       end
